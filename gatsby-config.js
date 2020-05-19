64f777b0952402f716a146f77cd99f7872678eae
JacqKevin <jacq.kevin@hotmail.fr>
2020-05-19 14:56:29 +0200

    Delete gatsby-config.js

--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Hyperspectral imaging toolboxes',
-    shortName: 'HSI toolboxes',
-    description: 'by Kévin Jacq',
-  },
-  plugins: ['@primer/gatsby-theme-doctocat'],
-  pathPrefix: '/JacqKevin.github.io',
-}
