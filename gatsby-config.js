0af000329c4460925b0ac329b72b272938660580
JacqKevin <jacq.kevin@hotmail.fr>
2020-05-19 13:57:36 +0200

    Create gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Hyperspectral imaging toolboxes',
+    shortName: 'HSI toolboxes',
+    description: 'by Kévin Jacq',
+  },
+  plugins: ['@primer/gatsby-theme-doctocat'],
+}
